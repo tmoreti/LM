a7d717a3c84a7580299e62ca27c5f245f3d073d9
tmoreti <thiago.moreti@gmail.com>
2017-01-05 22:16:05 -0200

    camera

---
+++ platforms/android/assets/www/assets/js/cadastroBandas.js
@@ -0,0 +1,32 @@
+function teste(){
+	alert('teste')
+}
+function selecionarFoto(){
+	navigator.camera.getPicture(uploadPhoto, function(message) {
+	 	alert('get picture failed');
+	}, {
+	 quality: 100,
+	 destinationType: navigator.camera.DestinationType.FILE_URI,
+	 sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
+	});
+}
+
+function uploadPhoto(imageURI) {
+ var options = new FileUploadOptions();
+ options.fileKey = "file";
+ options.fileName = imageURI.substr(imageURI.lastIndexOf('/') + 1);
+ options.mimeType = "image/jpeg";
+ console.log(options.fileName);
+ var params = new Object();
+ params.value1 = "test";
+ params.value2 = "param";
+ options.params = params;
+ options.chunkedMode = false;
+
+var ft = new FileTransfer();
+ ft.upload(imageURI, "http://192.168.1.4/phonegap/upload/upload.php", function(result){
+ console.log(JSON.stringify(result));
+ }, function(error){
+ console.log(JSON.stringify(error));
+ }, options);
+ }
