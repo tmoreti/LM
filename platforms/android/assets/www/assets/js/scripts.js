42d1faf60e02e8c9a17bf18e909811e7f7af5302
tmoreti <thiago.moreti@gmail.com>
2017-01-07 00:57:14 -0200

    correção js

--- platforms/android/assets/www/assets/js/scripts.js
+++ platforms/android/assets/www/assets/js/scripts.js
@@ -1,6 +1,6 @@
 var segmento='';
 var base;
-var site='http://livemusicapp.com.br/livemusicapp/';
+var site='https://livemusicapp.com.br/livemusicapp/';
 $( document ).ready(function() {
 	new WOW().init();
 	navigator.splashscreen.show();
