7f8fe15d1306fc24056c5436bfd8ca2630ac8c3c
tmoreti <thiago.moreti@gmail.com>
2017-01-26 23:37:33 -0200

    new

--- platforms/browser/www/js/config.js
+++ platforms/browser/www/js/config.js
@@ -10,7 +10,7 @@ function irpara(url){
 		$('.hd').fadeOut('slow');
 		$('#sidebar').fadeOut('slow');
 	}
-	if($('#sidebar').css('left')==0){
+	if($('#sidebar').css('left')=='0px'){
 		toggle_sidebar();
 	}
 	$.mobile.changePage(url, { transition: "slide", changeHash: false });
